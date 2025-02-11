33a0bc85736c811594ed62a02c0a36eb36787363
carljohn222 <carlpastrana44@gmail.com>
2025-02-11 11:14:11 +0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -8,10 +8,10 @@ function App() {
     <>
     <div className="flex flex-col items-center justify-between">
       <h1  className='text-4xl font-bold mb-4 mt-10 text-center'>To-do list</h1>
-      <Task label="Complete React project" />
-      <Task label="Learn Tailwind CSS" />
+      <Task label="Learn React " />
+      <Task label="To Do list" />
       <Task label="Master Javacscript" />
-      <Task label="Deploy the app" />
+      <Task label="Apply for jobs" />
       <div className='flex gap-5 items-end justify-end w-250'>
        <Add/>
        <Delete/>
